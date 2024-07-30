e42ece9210a4af26dd00a8a84f64c823ed54c09a
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-07-30 10:47:29 +0200

    Logging in

--- mod.ts
+++ mod.ts
@@ -3,3 +3,61 @@
  * platform. It comes with an easy way to connect to Meower and parse commands.
  * @module
  */
+
+import { z } from "npm:zod";
+
+const LOGIN_SCHEMA = z.discriminatedUnion("error", [
+  z.object({ error: z.literal(false), token: z.string() }),
+  z.object({ error: z.literal(true), type: z.string() }),
+]);
+const AUTH_PACKET_SCHEMA = z.object({
+  cmd: z.literal("auth"),
+  val: z.object({ token: z.string() }),
+});
+
+/**
+ * A bot connecting to Meower.
+ */
+export class RoarBot {
+  /**
+   * Log into an account and start the bot.
+   * @param username The username of the account the bot should log into.
+   * @param password The password of the account the bot should log into. This can also be a token that will get invalidated when the login succeeds.
+   * @throws When the login fails.
+   * @example
+   * ```js
+   * const bot = new RoarBot();
+   * bot.login("BearBot", "12345678");
+   * ```
+   * > [!NOTE]
+   * > In a real scenario, the password should not be in plain text like this,
+   * > but in an environment variable.
+   */
+  async login(username: string, password: string) {
+    const response = LOGIN_SCHEMA.parse(
+      await (
+        await fetch(`https://api.meower.org/auth/login`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ username, password }),
+        })
+      ).json()
+    );
+    if (response.error) {
+      throw new Error(
+        `Couldn't log in: ${response.type}. Ensure that you have the correct password!`
+      );
+    }
+    const ws = new WebSocket(
+      `https://server.meower.org?v=1&token=${response.token}`
+    );
+    ws.addEventListener("message", ({ data }) => {
+      const parsed = AUTH_PACKET_SCHEMA.safeParse(JSON.parse(data));
+      if (!parsed.success) {
+        return;
+      }
+      const token = parsed.data.val.token;
+      console.log(`Your token is ${token}`);
+    });
+  }
+}
