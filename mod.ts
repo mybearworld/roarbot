d321166c416aa82697f1fc4b1d5b1cca5651cfc3
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-08-03 21:56:33 +0200

    1.5.3

--- mod.ts
+++ mod.ts
@@ -76,7 +76,7 @@ import {
 export type { Post, UploadsAttachment, Attachment, User } from "./types.ts";
 
 const ATTACMHENT_MAX_SIZE = 25 << 20;
-const version = "1.5.2";
+const version = "1.5.3";
 
 /**
  * A bot connecting to Meower.
