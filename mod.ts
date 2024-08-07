fba88b6def813be78c5e9c3c1fd24846bd7b5c7b
mybearworld <130385691+mybearworld@users.noreply.github.com>
2024-08-07 14:37:47 +0200

    1.6.0

--- mod.ts
+++ mod.ts
@@ -86,7 +86,7 @@ export * from "./patterns.ts";
 export * from "./rich/post.ts";
 
 const ATTACMHENT_MAX_SIZE = 25 << 20;
-const version = "1.5.3";
+const version = "1.6.0";
 const logTimeFormat = new Intl.DateTimeFormat("en-US", {
   timeStyle: "medium",
   hour12: false,
